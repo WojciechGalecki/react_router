7e9370230ab7b37cb44469b566725e22f34f111d
Wojciech Galecki <wojciech.galecki@wp.pl>
2024-12-18 14:29:29 +0100

    useNavigate example

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h1>Home Page</h1>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 }
